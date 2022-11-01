c238f51425fa70a906dce20b9de8553153554be5
thang26 <ducthang9146@gmail.com>
2022-11-01 21:17:01 +0700

    fix task-router and handle error

--- be/routers/task.router.js
+++ be/routers/task.router.js
@@ -4,9 +4,9 @@ import taskController from "../controllers/task.controller.js";
 const taskRouter = express.Router();
 
 taskRouter.get("/", taskController.getAllTasks);
-taskRouter.get("/", taskController.getTaskById);
+taskRouter.get("/:id", taskController.getTaskById);
 taskRouter.post("/", taskController.createTask);
-taskRouter.patch("/", taskController.updateTask);
-taskRouter.delete("/", taskController.deleteTask);
+taskRouter.patch("/:id", taskController.updateTask);
+taskRouter.delete("/:id", taskController.deleteTask);
 
 export default taskRouter;
