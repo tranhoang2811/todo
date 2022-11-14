ab8a15fa366be77a507f6781d9735e9332275233
Trần Hoàng <trandanghoang2811@gmail.com>
2022-11-14 15:08:39 +0700

    create validate function

--- fe/src/pages/RegisterPage/index.jsx
+++ fe/src/pages/RegisterPage/index.jsx
@@ -4,15 +4,16 @@ import ActionGroup from "./components/ActionGroup/index.jsx";
 import GenderInput from "./components/GenderInput/index.jsx";
 import AgeInput from "./components/AgeInput/index.jsx";
 import "./style.css";
+import "../../assets/fonts/themify-icons/themify-icons.css";
 
 function RegisterPage() {
   return (
     <div className="register">
       <Header />
       <div className="register__description">
-        <InformationInput type={"fullName"} />
-        <InformationInput type={"email"} />
-        <InformationInput type={"password"} />
+        <InformationInput type={"fullName"} iconType={"ti-user"} />
+        <InformationInput type={"email"} iconType={"ti-email"} />
+        <InformationInput type={"password"} iconType={"ti-lock"} />
         <div className="age-and-gender">
           <GenderInput />
           <AgeInput />
