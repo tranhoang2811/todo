728f921497af9823b6bea6228f938044610dd45f
Trần Hoàng <trandanghoang2811@gmail.com>
2022-11-18 21:04:12 +0700

    authorize

--- be/middleware/authentication.js
+++ be/middleware/authentication.js
@@ -1,15 +1,16 @@
 import Jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
-function authenticate(req, res) {
-  if (req.header.authorization !== "") {
-    const token = req.body.authorization.split(" ")[1];
+function authenticate(req, res, next) {
+  if (req.headers.authorization !== "") {
+    const token = req.headers.authorization.split(" ")[1];
     const decodedToken = Jwt.verify(token, "SECRET_KEY");
     return User.findById(decodedToken.id).then((user) => {
       if (user === null) {
         res.end("User not found");
       }
       req.user = decodedToken;
+      next();
     });
   }
 }
--- be/routers/task.router.js
+++ be/routers/task.router.js
@@ -1,12 +1,43 @@
 import express from "express";
 import taskController from "../controllers/task.controller.js";
+import authorize from "../middleware/authorization.js";
+import authenticate from "../middleware/authentication.js";
 
 const taskRouter = express.Router();
 
-taskRouter.get("/", taskController.getAllTasks);
-taskRouter.get("/:id", taskController.getTaskById);
-taskRouter.post("/", taskController.createTask);
-taskRouter.patch("/:id", taskController.updateTask);
-taskRouter.delete("/:id", taskController.deleteTask);
+taskRouter.get(
+  "/",
+  authenticate,
+  authorize("readOwn", "task"),
+  taskController.getAllTasks
+);
+
+taskRouter.get(
+  "/:id",
+  authenticate,
+  authorize("readOwn", "task"),
+  taskController.getTaskById
+);
+
+taskRouter.post(
+  "/",
+  authenticate,
+  authorize("createOwn", "task"),
+  taskController.createTask
+);
+
+taskRouter.patch(
+  "/:id",
+  authenticate,
+  authorize("updateOwn", "task"),
+  taskController.updateTask
+);
+
+taskRouter.delete(
+  "/:id",
+  authenticate,
+  authorize("deleteOwn", "task"),
+  taskController.deleteTask
+);
 
 export default taskRouter;
